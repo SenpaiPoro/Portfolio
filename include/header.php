aa37f5b656094ae38abd3dc078bd1998068069f8
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-03-23 01:12:19 +0800

    sync the foler assets for pfp

--- include/header.php
+++ include/header.php
@@ -1,3 +1,12 @@
+<?php
+require '../config/function.php';
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}if (!isset($_SESSION['id'])) {
+  header('Location: login.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
