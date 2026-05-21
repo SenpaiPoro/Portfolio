c18c665887d24b383484532872f3005bbcf071f6
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-05-21 15:30:27 +0800

    link the button into add project

--- projects.php
+++ projects.php
@@ -36,7 +36,7 @@
                     else
                     {
                         ?>
-                        <h2>No Project Found <span class="badge text-bg-secondary">Add</span></h2>
+                        <h2>No Project Found <span class="badge text-bg-secondary"><a href="../Portfolio_Dashboard/project.php">Add</a></span></h2>
                     <?php
 
                     }?>
