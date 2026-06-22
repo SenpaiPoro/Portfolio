b1da410072ab0ce7c99779c8b4a476e86a5414c2
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-06-23 04:30:43 +0800

    will display the data of skills on the resume file

--- resume.php
+++ resume.php
@@ -103,34 +103,31 @@
                                             <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline ">Professional Skills</span></h3>
                                         </div>
                                         <div class="row row-cols-1 row-cols-md-3 mb-3">
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">SEO/SEM Marketing</div></div>
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Statistical Analysis</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Web Development</div></div>
+
+
+
+                                            <?php 
+                                            $professionalSkills = Getdata("skills", $id, "SKILL");
+
+                                            foreach ($professionalSkills as $skill): ?>
+                                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode"><?= $skill ?></div></div>
+                                            <?php endforeach; ?>
+                                        </div>
                                     </div>
                                     <!-- Languages list-->
                                     <div class="mb-0">
-                                        <div class="d-flex align-items-center mb-4">
+                                        <div class="d-flex align-items-center mb-4">x
                                             <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
                                             <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
                                         </div>
                                         <div class="row row-cols-1 row-cols-md-3 mb-4">
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">HTML</div></div>
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">CSS</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">JavaScript</div></div>
-                                        </div>
-                                        <div class="row row-cols-1 row-cols-md-3">
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Python</div></div>
-                                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Ruby</div></div>
-                                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode">Node.js</div></div>
-                                        </div>
+                                            <?php 
+                                            $languages = Getdata("skills", $id, "LANGUAGE");
+
+                                            foreach ($languages as $language): ?>
+                                                <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100 text-dark-mode"><?= $language ?></div></div>
+                                            <?php endforeach; ?>
+                                        </div>`
                                     </div>
                                 </div>
                             </div>
