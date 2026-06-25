26d7e032c52e6dc81ff20bd119cfbc64e978d0a1
Marlon Jr Mullet <marlonmullet10@gmail.com>
2026-06-26 03:44:27 +0800

    remove the link button for adding project

--- projects.php
+++ projects.php
@@ -61,7 +61,7 @@
                     else
                     {
                         ?>
-                        <h2 class="text-dark-mode" >No Project Found <span class="badge text-bg-secondary"><a href="../Portfolio_Dashboard/project.php">Add </a></span></h2>
+                        <h2 class="text-dark-mode" >No Project Found </h2>
                     <?php
 
                     }?>
